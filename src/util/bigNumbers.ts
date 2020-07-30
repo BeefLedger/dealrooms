f093acc0719ccc61e73f0261a81f1d839ddf0f07
Bazmati <barry@earsman.com>
2020-07-31 09:29:14 +1000

    Added handy BigNumber utils

---
+++ src/util/bigNumbers.ts
@@ -0,0 +1,9 @@
+import { BigNumber, BigNumberish } from "ethers";
+
+export function bnEquals(first: BigNumberish, other: BigNumberish): boolean {
+    return (BigNumber.from(first).toHexString() == BigNumber.from(other).toHexString())
+}
+
+export function bnToNumber(bn: BigNumberish): number {
+    return BigNumber.from(bn).toNumber()
+}
